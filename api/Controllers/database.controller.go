bc09d947d4f60f0f3be9ccbb79178ac25c691ce9
Thodoris Anagnostopoulos <thodorisanagnostopoulos0@gmail.com>
2023-01-18 16:40:41 +0200

    fixed database

--- api/Controllers/database.controller.go
+++ api/Controllers/database.controller.go
@@ -22,7 +22,7 @@ func (server *Server) Initialize(Db_user, Db_password, Db_host, Db_name, Db_port
 	var err error
 
 	//dsn := Db_user + ":" + Db_password + "@tcp" + "(" + Db_host + ":" + Db_port + ")/" + Db_name + "?" + "parseTime=true&loc=Local"
-	dsn := os.Getenv("DSN_NAME") + ":" + os.Getenv("DSN_PASSWORD") + "@tcp(aws-eu-west-2.connect.psdb.cloud)/connectly_project?tls=true"
+	dsn := os.Getenv("DSN_NAME") + ":" + os.Getenv("DSN_PASSWORD") + "@tcp(aws-eu-west-2.connect.psdb.cloud)/connectly_project?tls=true&parseTime=true&loc=Local"
 	server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
